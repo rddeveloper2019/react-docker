f4503bd41a167e59086dc489b27dfce6510d1104
Rid Van <fatzey2010@gmail.com>
2025-01-27 23:47:37 +0300

    crash test

--- src/App.test.js
+++ src/App.test.js
@@ -6,13 +6,8 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-test('renders learn react link', () => {
+test('must crush', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(8).toEqual(10);
 });
